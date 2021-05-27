42139c4d8db9c9a62ad4cfd014668e2d8aefb05f
Per <atlasfreak@gmx.de>
2021-05-27 05:01:14 +0200

    added copyright disclaimer to info.php;

--- info.php
+++ info.php
@@ -6,11 +6,28 @@
  * @version         0.1
  * @authors         Per Göttlicher
  * @copyright       2021-, Per Göttlicher (Atlasfreak)
- * @link
+ * @link            https://github.com/Atlasfreak/WBCE_FullCalendar
  * @license         https://www.gnu.org/licenses/gpl-3.0.html
  * @platform        WBCE 1.5.x
  *
  **/
+/*
+ * Copyright (C) 2021  Per Göttlicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
 
 $module_directory   = 'fullcalendar';
 $module_name        = 'FullCalendar Module';
